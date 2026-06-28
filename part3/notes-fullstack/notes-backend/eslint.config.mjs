15c566299839025bdfa4c3a2585067e4f70f32a2
BlackFace <zzyouxiang@gmail.com>
2026-06-28 21:49:18 +0800

    feat：part3c修复lint配置

--- part3/notes-fullstack/notes-backend/eslint.config.mjs
+++ part3/notes-fullstack/notes-backend/eslint.config.mjs
@@ -1,11 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { 
+    ignores: ['dist/**'], 
+  },
 ]);
