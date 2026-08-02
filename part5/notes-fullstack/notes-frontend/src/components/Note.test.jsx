263b3cd0a6c8a5db5008583e62b869886d1a95a9
BlackFace <zzyouxiang@gmail.com>
2026-08-03 00:57:32 +0800

    feat:part5c2-5提交

--- part5/notes-fullstack/notes-frontend/src/components/Note.test.jsx
+++ part5/notes-fullstack/notes-frontend/src/components/Note.test.jsx
@@ -25,6 +25,11 @@ test('renders content', () => {
 
   render(<Note note={note} />)
 
+  // screen.debug()
+
   const element = screen.getByText('Component testing is done with react-testing-library')
+  
+  // screen.debug(element)
+
   expect(element).toBeDefined()
 })
