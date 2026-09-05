d366ff3ce282436f0083e1a7e46496105c595894
BlackFace <zzyouxiang@gmail.com>
2026-09-05 23:28:48 +0800

    feat:9-3-Creating your first own types

--- part9/chapter3/multiplicator.ts
+++ part9/chapter3/multiplicator.ts
@@ -1,37 +1,25 @@
-interface MultiplyValues {
-  value1: number;
-  value2: number;
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) : number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
 }
 
-const parseArguments = (args: string[]): MultiplyValues => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      value1: Number(args[2]),
-      value2: Number(args[3]),
-    };
-  } else {
-    throw new Error("Provided values were not numbers!");
-  }
-};
-
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText, a * b);
-};
-
 try {
-  const { value1, value2 } = parseArguments(process.argv);
-  multiplicator(
-    value1,
-    value2,
-    `Multiplied ${value1} and ${value2}, the result is:`
-  );
+  console.log(calculator(1, 5 , 'divide'));
 } catch (error: unknown) {
-  let errorMessage = "Something bad happened.";
+  let errorMessage = 'Something went wrong: '
   if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
+    errorMessage += error.message;
   }
   console.log(errorMessage);
 }
